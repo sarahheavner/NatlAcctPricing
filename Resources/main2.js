76a700cc8312e77f36935549912b9859d6df8c64
Sarah Heavner <sheavner@welcheq.com>
2021-12-14 15:11:00 -0700

    fixed javascript table error, now pulls data from csv. broke html dropdown menu

--- Resources/main2.js
+++ Resources/main2.js
@@ -1,11 +1,8 @@
-//connect to StatsSummary csv, read data
 d3.csv("Resources/CleanData/StatSummary.csv").then(function(tData) {
     console.log(tData);
 
-    //select tablebody to append stats
     var tbody = d3.select("tbody");
 
-    //loop through data, collect keys and values from each Object and append to table rows/cells
     tData.forEach(function(stats) {
         var row = tbody.append("tr");
         Object.entries(stats).forEach(function([key, value]) {
@@ -16,6 +13,4 @@ d3.csv("Resources/CleanData/StatSummary.csv").then(function(tData) {
         });
     });
 });
-
-
 
