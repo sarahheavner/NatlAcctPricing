6ca1f0fdac93c952a7ed0e4bb5132d830d32fb67
Sarah Heavner <sheavner@welcheq.com>
2021-12-15 13:51:12 -0700

    setting up inv stats pie chart

--- Resources/main.js
+++ Resources/main.js
@@ -10,7 +10,6 @@ d3.csv("Resources/CleanData/StatSummary.csv").then(function(tData) {
         var row = tbody.append("tr");
         Object.entries(stats).forEach(function([key, value]) {
             console.log(key, value);
-
             var cell = row.append("td");
             cell.text(value);
         });
@@ -18,4 +17,32 @@ d3.csv("Resources/CleanData/StatSummary.csv").then(function(tData) {
 });
 
 
+// inv stat pie chart 
+d3.csv("Resources/CleanData/StatSummary.csv").then(function(pieData) {
+    console.log(pieData);
+
+    // create variable for each branch total invoices
+    var ABQ_INV = [];
+    var DEN_INV = [];
+    var SLC_INV = [];
+
+    // loop through csv rows and store Total_Inv into variables based on Branch
+    for (var i = 0; i < pieData.length; i++) {
+        if (pieData[i].Branch === "ABQ") {
+            ABQ_INV.push(pieData[i].Total_Inv);
+            console.log(ABQ_INV)
+        };
+        if (pieData[i].Branch === "DEN") {
+            DEN_INV.push(pieData[i].Total_Inv);
+            console.log(DEN_INV)
+        };
+        if (pieData[i].Branch === "SLC") {
+            SLC_INV.push(pieData[i].Total_Inv);
+            console.log(SLC_INV)
+        };
+    };
+
+
+
+});
 
