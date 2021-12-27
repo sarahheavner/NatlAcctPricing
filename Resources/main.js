43ed252ba9bd86a45a4c2c1b8cb45a799e6024a7
Sarah Heavner <sheavner@welcheq.com>
2021-12-27 14:50:23 -0700

    added stacked bar chart to main html page

--- Resources/main.js
+++ Resources/main.js
@@ -139,3 +139,85 @@ d3.csv("Resources/CleanData/StatSummary.csv").then(function(invData) {
     Plotly.newPlot("bar", barData, barlayout);
 
 });
+
+
+//data for dollar summary
+d3.csv("Resources/CleanData/BranchDollarSummary.csv").then(function(dollarData) {
+    console.log(dollarData);
+
+    var branch_name = [];
+    var january = [];
+    var february = [];
+    var march = [];
+    var april = [];
+    var may = [];
+    var june = [];
+    var july= [];
+    var august = [];
+    var september = [];
+    var october = [];
+    var november = [];
+    var december = [];
+
+    for (var i = 0; i < dollarData.length; i++) {
+        branch_name.push(dollarData[i].Branch);
+        january.push(dollarData[i].January);
+        february.push(dollarData[i].February);
+        march.push(dollarData[i].March);
+        april.push(dollarData[i].April);
+        may.push(dollarData[i].May);
+        june.push(dollarData[i].June);
+        july.push(dollarData[i].July);
+        august.push(dollarData[i].August);
+        september.push(dollarData[i].September);
+        october.push(dollarData[i].October);
+        november.push(dollarData[i].November);  
+        december.push(dollarData[i].December);
+    };
+
+    console.log(branch_name);
+    console.log(january);
+    console.log(february);
+    console.log(march);
+    console.log(april);
+    console.log(may);
+    console.log(june);
+    console.log(july);
+    console.log(august);
+    console.log(september);
+    console.log(october);
+    console.log(november);
+    console.log(december);
+
+
+    var traceabq = {
+        x: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        y: [january[0], february[0], march[0], april[0], may[0], june[0], july[0], august[0], september[0], october[0], november[0], december[0]],
+        name: "ABQ",
+        type: "bar"
+    };
+
+    var traceden = {
+        x: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        y: [january[1], february[1], march[1], april[1], may[1], june[1], july[1], august[1], september[1], october[1], november[1], december[1]],
+        name: "DEN",
+        type: "bar"
+    };
+
+    var traceslc = {
+        x: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        y: [january[2], february[2], march[2], april[2], may[2], june[2], july[2], august[2], september[2], october[2], november[2], december[2]],
+        name: "SLC",
+        type: "bar"
+    };
+
+    var stackedData = [traceabq, traceden, traceslc];
+
+    layout = {
+        barmode: "stack"
+    };
+
+    Plotly.newPlot("bar-stacked", stackedData, layout)
+
+
+});
